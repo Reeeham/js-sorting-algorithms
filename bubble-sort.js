e339e2935b5e15f77f90f697268645c48186059e
reeeham <reeehamgamal@gmail.com>
2025-12-29 09:43:49 +0200

    bubble sort

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,41 +1,14 @@
-const bubbleSort = (arr) => {
-    let changed;
-    let temp;
-
-    do {
-        changed = false;
-        for(let i = 0; i < arr.length - 1; i++) {
-            if(arr[i] > arr[i+1]) {
-                temp = arr[i];
-                arr[i] = arr[i+1];
-                arr[i+1] = temp;
-                changed = true;
+function bubbleSort(arr) {
+    for(let i = 0; i < arr.length - 1; i++) {
+        for(let j = 0; j < arr.length - i - 1; j++) { 
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j];
+                 arr[j] = arr[j+1];
+                arr[j+1] = temp; 
             }
         }
-    } while(changed);
-}
-const swap = (arr, i, j) => {
-    let temp = arr[i];
-    arr[i] = arr[j];
-    arr[j] = temp;
-}
-
-const optimizedBubbleSort = (arr) => {
-    let isSorted = false;
-    let lastUnsorted = arr.length - 1;
-    while(!isSorted) {
-        isSorted = true;
-        for(let i = 0; i< lastUnsorted; i++) {
-            if(arr[i] > arr[i + 1]) {
-                swap(arr, i, i + 1);
-                isSorted = false;
-            }
-        }
-        lastUnsorted--;
     }
     return arr;
 }
 
-let inputArr = [5, 2, 4, 6, 1, 3];
-optimizedBubbleSort(inputArr);
-console.log(inputArr); // O(n^2)
+console.log(bubbleSort([11, 6, 4, 1, 0,10,4,6,7,9,3,2,19]))
